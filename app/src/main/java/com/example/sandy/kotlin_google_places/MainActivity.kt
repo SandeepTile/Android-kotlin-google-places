3ee2c8f91daef8f69c7d1b77f22718d6f727a93b
sandeeptile <sandeeptile@gmail.com>
2018-08-18 15:50:52 +0530

    current location by network provider

--- app/src/main/java/com/example/sandy/kotlin_google_places/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_google_places/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.example.sandy.kotlin_google_places
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var lati:Double? = null
+    var longi:Double? = null
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var lManager = getSystemService(Context.LOCATION_SERVICE)
+                as LocationManager
+        lManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                1000.toLong(),1.toFloat(), object : LocationListener {
+            override fun onLocationChanged(location: Location?) {
+
+                lati = location!!.latitude
+                longi = location!!.longitude
+                tv_lati.text = lati.toString()
+                tv_longi.text = longi.toString()
+                lManager.removeUpdates(this) //for stop updating location
+
+
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 }
