63ea714e7ca0b8cd0ab99bb1eb58fff6eca02843
sandeeptile <sandeeptile@gmail.com>
2018-08-18 16:16:10 +0530

    location picker done

--- app/src/main/java/com/example/sandy/kotlin_google_places/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_google_places/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.sandy.kotlin_google_places
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.places.ui.PlacePicker
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var lati:Double? = null
-    var longi:Double? = null
+    var lati: Double? = null
+    var longi: Double? = null
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
                 as LocationManager
         lManager.requestLocationUpdates(
                 LocationManager.NETWORK_PROVIDER,
-                1000.toLong(),1.toFloat(), object : LocationListener {
+                1000.toLong(), 1.toFloat(), object : LocationListener {
             override fun onLocationChanged(location: Location?) {
 
                 lati = location!!.latitude
@@ -48,5 +50,23 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        //location picker (manually)
+        loc_pin.setOnClickListener {
+
+            val builder = PlacePicker.IntentBuilder()
+            startActivityForResult(builder.build(this@MainActivity),
+                    1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val place = PlacePicker.getPlace(data!!, this)
+        lati = place.latLng.latitude
+        longi = place.latLng.longitude
+        tv_lati.text = lati.toString()
+        tv_longi.text = longi.toString()
+
     }
 }
